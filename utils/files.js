668cd4afa23fb90b300fcbf9b3fc54c78eacb917
Venkat.R <ramsunvtech@gmail.com>
2022-03-20 00:54:18 +0800

    tech(base): add util to sanitize and create directory and file

--- utils/files.js
+++ utils/files.js
@@ -1,21 +1,36 @@
 const shell = require('shelljs')
 const sanitize = require("sanitize-filename")
 
+function toPascalCase(input){
+  return `${input}`
+    .replace(new RegExp(/[-_]+/, 'g'), ' ')
+    .replace(new RegExp(/[^\w\s]/, 'g'), '')
+    .replace(
+      new RegExp(/\s+(.)(\w+)/, 'g'),
+      ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+    )
+    .replace(new RegExp(/\s/, 'g'), '')
+    .replace(new RegExp(/\w/), s => s.toUpperCase());
+}
+
 function createDirectory(pathWithName) {
   return shell.mkdir('-p', pathWithName)
 }
 
-function sanitizeName(directoryOrFileName) {
+function sanitizeName(directoryOrFileName, options = {}) {
+  const { replaceWith = '-', skipReplaceOnFirstItem = true, pascalCase = false } = options || {}
   const sanitized = sanitize(directoryOrFileName)
   const lowerCased = sanitized.toLowerCase()
-  const hyphenate = true
   let validName = ''
 
   lowerCased.split(' ').forEach((subString, index) => {
-    if (index === 0) {
-      validName = subString.trim()
-    } else if (hyphenate && subString.trim() !== '' && index > 0) {
-      validName += `-${subString.trim()}`
+    const trimmedSubString = subString.trim()
+    if (pascalCase) {
+      validName += toPascalCase(trimmedSubString)
+    } else if (skipReplaceOnFirstItem && index === 0) {
+      validName += trimmedSubString
+    } else if (trimmedSubString !== '' && replaceWith !== '') {
+      validName += `${replaceWith}${trimmedSubString}`
     }
   })
 
