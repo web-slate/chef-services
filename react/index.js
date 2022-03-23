32cdc5b81ac8493bb52b99230e0432b8c741bcec
Venkat.R <ramsunvtech@gmail.com>
2022-03-24 00:44:01 +0800

    tech(base): support react api base method

---
+++ react/index.js
@@ -0,0 +1,21 @@
+const createGetServiceFile = require('./get')
+const createPostServiceFile = require('./post')
+const createPutServiceFile = require('./put')
+const createDeleteServiceFile = require('./delete')
+
+module.exports = function createServiceFile (options) {
+  const { name, method } = options
+  const creatorMapping = {
+    GET: createGetServiceFile,
+    POST: createPostServiceFile,
+    PUT: createPutServiceFile,
+    DELETE: createDeleteServiceFile,
+  }
+  const serviceCreator = creatorMapping[method]
+
+  if (serviceCreator) {
+    return serviceCreator(options)
+  }
+
+  return `export default function ${name}() { }`
+}
