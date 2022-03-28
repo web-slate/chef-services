694d9706c3575c0dedbb471dbf184f000ad17808
Venkat.R <ramsunvtech@gmail.com>
2022-03-29 03:48:44 +0800

    fix(react/delete/services): update sample implementation snippet

--- react/delete.js
+++ react/delete.js
@@ -1,4 +1,6 @@
-module.exports = function createDeleteServiceFile ({ name, endpoint }) {
+module.exports = function createDeleteServiceFile ({ name, serviceName, endpoint, body }) {
+const camelCasedServiceName = serviceName.camelCased
+
 return `import useDelete from '../../useDelete'
 
 export default function ${name}(methodName) {
@@ -8,5 +10,37 @@ export default function ${name}(methodName) {
   )
   return { response, error, loading, [methodName]: sendDeleteData }
 }
+
+/***
+ * Sample Implementation for above Delete Services Custom Hook.
+
+// Services
+import ${name} from './path/to/${name}'
+
+// Inside your component.
+const {
+  response: ${camelCasedServiceName}Response,
+  error: ${camelCasedServiceName}Error,
+  loading: ${camelCasedServiceName}Loading,
+  sendDeleteData: ${camelCasedServiceName}PostedData
+} = ${name}(
+  'sendDeleteData'
+)
+
+const ${camelCasedServiceName}Payload = ${body}
+
+return (
+  <pre>
+    <pre>{JSON.stringify(${camelCasedServiceName}Payload)}</pre>
+    <div><button type="button" onClick={()=>{${camelCasedServiceName}PostedData(${camelCasedServiceName}Payload)}}>${serviceName.default}</button></div>
+    <div>Response:</div>
+    <pre>
+      {${camelCasedServiceName}Loading && 'Delete ${camelCasedServiceName} Loading...'}
+      {${camelCasedServiceName}Error && \`${camelCasedServiceName} Error: \$\{${camelCasedServiceName}Error.message\}\`}
+      {${camelCasedServiceName}Response && JSON.stringify(${camelCasedServiceName}Response)}
+    </pre>
+  </pre>
+)
+*/
 `
 }
