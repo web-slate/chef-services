eb668c2b87f7b07388076f47042d36453b883147
Venkat.R <ramsunvtech@gmail.com>
2022-03-29 03:41:15 +0800

    fix(react/post/services): update sample implementation snippet

--- react/post.js
+++ react/post.js
@@ -1,5 +1,6 @@
-module.exports = function createPostServiceFile ({ name, endpoint }) {
-return `import usePost from '../../usePost'
+module.exports = function createPostServiceFile({ name, serviceName, endpoint, body }) {
+  const camelCasedServiceName = serviceName.camelCased
+  return `import usePost from '../../usePost'
 
 export default function ${name}(methodName) {
   const { response, error, loading, sendPostData } = usePost(
@@ -8,5 +9,37 @@ export default function ${name}(methodName) {
   )
   return { response, error, loading, [methodName]: sendPostData }
 }
+
+/***
+ * Sample Implementation for above Post Services Custom Hook.
+
+// Services
+import ${name} from './path/to/${name}'
+
+// Inside your component.
+const {
+  response: ${camelCasedServiceName}Response,
+  error: ${camelCasedServiceName}Error,
+  loading: ${camelCasedServiceName}Loading,
+  sendPostData: ${camelCasedServiceName}PostedData
+} = ${name}(
+  'sendPostData'
+)
+
+const ${camelCasedServiceName}Payload = ${body}
+
+return (
+  <pre>
+    <pre>{JSON.stringify(${camelCasedServiceName}Payload)}</pre>
+    <div><button type="button" onClick={()=>{${camelCasedServiceName}PostedData(${camelCasedServiceName}Payload)}}>${serviceName.default}</button></div>
+    <div>Response:</div>
+    <pre>
+      {${camelCasedServiceName}Loading && 'Post ${camelCasedServiceName} Loading...'}
+      {${camelCasedServiceName}Error && \`${camelCasedServiceName} Error: \$\{${camelCasedServiceName}Error.message\}\`}
+      {${camelCasedServiceName}Response && JSON.stringify(${camelCasedServiceName}Response)}
+    </pre>
+  </pre>
+)
+*/
 `
 }
