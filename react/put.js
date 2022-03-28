d6e16cc8b5b4f998b7ae94ee2b3e8b965e39c2cf
Venkat.R <ramsunvtech@gmail.com>
2022-03-29 03:41:33 +0800

    fix(react/put/services): update sample implementation snippet

--- react/put.js
+++ react/put.js
@@ -1,4 +1,6 @@
-module.exports = function createPutServiceFile ({ name, endpoint }) {
+module.exports = function createPutServiceFile ({ name, serviceName, endpoint, body }) {
+const camelCasedServiceName = serviceName.camelCased
+
 return `import usePut from '../../usePut'
 
 export default function ${name}(methodName) {
@@ -8,5 +10,37 @@ export default function ${name}(methodName) {
   )
   return { response, error, loading, [methodName]: sendPutData }
 }
+
+/***
+ * Sample Implementation for above Put Services Custom Hook.
+
+// Services
+import ${name} from './path/to/${name}'
+
+// Inside your component.
+const {
+  response: ${camelCasedServiceName}Response,
+  error: ${camelCasedServiceName}Error,
+  loading: ${camelCasedServiceName}Loading,
+  sendPutData: ${camelCasedServiceName}PostedData
+} = ${name}(
+  'sendPutData'
+)
+
+const ${camelCasedServiceName}Payload = ${body}
+
+return (
+  <pre>
+    <pre>{JSON.stringify(${camelCasedServiceName}Payload)}</pre>
+    <div><button type="button" onClick={()=>{${camelCasedServiceName}PostedData(${camelCasedServiceName}Payload)}}>${serviceName.default}</button></div>
+    <div>Response:</div>
+    <pre>
+      {${camelCasedServiceName}Loading && 'Put ${camelCasedServiceName} Loading...'}
+      {${camelCasedServiceName}Error && \`${camelCasedServiceName} Error: \$\{${camelCasedServiceName}Error.message\}\`}
+      {${camelCasedServiceName}Response && JSON.stringify(${camelCasedServiceName}Response)}
+    </pre>
+  </pre>
+)
+*/
 `
 }
