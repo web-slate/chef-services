2be41d0441ab0d7f8edb88d083f44c5a02c04f44
Venkat.R <ramsunvtech@gmail.com>
2022-03-29 21:45:59 +0800

    fix(error): add try catch block

--- generator.js
+++ generator.js
@@ -16,68 +16,74 @@ program
   .option('-u, --utilLocation <value>', 'location separated by slashes')
   .parse()
 
-const { postmanFile, location = 'hooks/services', utilLocation = 'utils' } = program.opts()
+try {
+  const { postmanFile, location = 'hooks/services', utilLocation = 'utils' } = program.opts()
 
-const FILE_EXTENSION = 'js'
-const DEFINITION_FILE = postmanFile
-
-if (!DEFINITION_FILE) {
-  error('Please provide a postman exported collection file.')
-}
-
-const postmanCollectionJson = require(path.resolve(DEFINITION_FILE))
-
-const { item: collections = [] } = postmanCollectionJson || {}
-const endpointList = []
-
-if (Array.isArray(collections)) {
-  collections.forEach(collectionItem => {
-    const directoryName = sanitizeName(collectionItem?.name)
-    createDirectory(
-      path.resolve(location, directoryName)
-    )
-
-    if (Array.isArray(collectionItem?.item)) {
-      collectionItem.item.forEach(apiItem => {
-        const pascalCasedServiceName = sanitizeAndPascalCase(apiItem?.name)
-        const customHookName = `use${pascalCasedServiceName}`
-        const customHookFile = `${customHookName}.${FILE_EXTENSION}`
-        const { method, url = {}, body = {} } = apiItem?.request || {}
-
-        if (!method || !url?.raw) {
-          return
-        }
-
-        const endpointName = sanitizeName(apiItem?.name, { replaceWith: '_'}).toUpperCase()
-        endpointList.push({
-          name: endpointName,
-          url
-        })
-
-        createFile({
-          pathToFile: path.resolve(location, directoryName, customHookFile),
-          data: createServiceFile({
-            name: customHookName,
-            serviceName: {
-              default: apiItem?.name,
-              camelCased: sanitizeAndCamelCase(apiItem?.name),
-              pascalCased: pascalCasedServiceName,
-            },
-            method,
-            endpoint: endpointName,
-            body: body?.raw || '' 
+  const FILE_EXTENSION = 'js'
+  const DEFINITION_FILE = postmanFile
+  
+  if (!DEFINITION_FILE) {
+    error('Please provide a postman exported collection file.')
+  }
+  
+  const postmanCollectionJson = require(path.resolve(DEFINITION_FILE))
+  
+  const { item: collections = [] } = postmanCollectionJson || {}
+  const endpointList = []
+  
+  if (Array.isArray(collections)) {
+    collections.forEach(collectionItem => {
+      const directoryName = sanitizeName(collectionItem?.name)
+      createDirectory(
+        path.resolve(location, directoryName)
+      )
+  
+      if (Array.isArray(collectionItem?.item)) {
+        collectionItem.item.forEach(apiItem => {
+          const pascalCasedServiceName = sanitizeAndPascalCase(apiItem?.name)
+          const customHookName = `use${pascalCasedServiceName}`
+          const customHookFile = `${customHookName}.${FILE_EXTENSION}`
+          const { method, url = {}, body = {} } = apiItem?.request || {}
+  
+          if (!method || !url?.raw) {
+            return
+          }
+  
+          const endpointName = sanitizeName(apiItem?.name, { replaceWith: '_'}).toUpperCase()
+          endpointList.push({
+            name: endpointName,
+            url
+          })
+  
+          createFile({
+            pathToFile: path.resolve(location, directoryName, customHookFile),
+            data: createServiceFile({
+              name: customHookName,
+              serviceName: {
+                default: apiItem?.name,
+                camelCased: sanitizeAndCamelCase(apiItem?.name),
+                pascalCased: pascalCasedServiceName,
+              },
+              method,
+              endpoint: endpointName,
+              body: body?.raw || '' 
+            })
           })
         })
-      })
-    }
-  })
+      }
+    })
+  
+    createDirectory(
+      path.resolve(location, utilLocation)
+    )
+  
+    createFile({
+      pathToFile: path.resolve(location, utilLocation, `endpoints.${FILE_EXTENSION}`),
+      data: createUtilFile(endpointList)
+    })
+  }
+  console.log('Service Files created. Happy Coding!')
 
-  createDirectory(
-    path.resolve(location, utilLocation)
-  )
-
-  createFile({
-    pathToFile: path.resolve(location, utilLocation, `endpoints.${FILE_EXTENSION}`),
-    data: createUtilFile(endpointList)
-  })
+} catch (err) {
+  console.log('Error while reading file.', err);
 }
